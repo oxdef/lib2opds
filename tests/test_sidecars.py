26e81f9bc6bcf27e5c4b88a49d256d08e12b9dd0
Taras Ivashchenko <oxdef@oxdef.info>
2025-02-08 15:26:25 +0100

    Add more tests for sidecar files

--- tests/test_sidecars.py
+++ tests/test_sidecars.py
@@ -5,12 +5,55 @@ import pytest
 from lib2opds.sidecars import (
     CoverSidecarFile,
     InfoSidecarFile,
-    MetadataSidecarFile,
     get_cover_sidecar_file,
     get_metadata_sidecar_file,
 )
 
 
-def test_metadata_sidecar_file() -> None:
-    info = get_metadata_sidecar_file(Path("sidecar"))
-    assert type(info) == InfoSidecarFile  # nosec B101
+class TestInfoSidecarFile:
+    test_sidecar_path = Path("tests").joinpath("data").joinpath("sidecar")
+
+    def test_factory(self) -> None:
+        info = get_metadata_sidecar_file(self.test_sidecar_path)
+        assert type(info) == InfoSidecarFile
+
+    def test_read(self) -> None:
+        info = get_metadata_sidecar_file(self.test_sidecar_path)
+        assert info.read() == True
+
+    def test_read_false(self) -> None:
+        info = get_metadata_sidecar_file(self.test_sidecar_path.with_name("nonexist"))
+        assert info.read() != True
+
+    def test_parsing(self) -> None:
+        info = get_metadata_sidecar_file(self.test_sidecar_path)
+        info.read()
+        assert info.title == "Some title"
+        assert info.description == "Some description"
+        assert info.authors == ["Some author"]
+
+    def test_write(self, tmp_path: Path) -> None:
+        test_sidecar_path = tmp_path / "somefile"
+        info = get_metadata_sidecar_file(test_sidecar_path)
+        info.title = "Some title"
+        info.description = "Some description"
+        info.authors = ["Some author"]
+        assert info.write() == True
+
+        info = get_metadata_sidecar_file(test_sidecar_path)
+        info.read()
+        assert info.title == "Some title"
+        assert info.description == "Some description"
+        assert info.authors == ["Some author"]
+
+
+class TestCoverSidecarFile:
+    test_sidecar_path = Path("tests").joinpath("data").joinpath("sidecar.cover")
+
+    def test_factory(self) -> None:
+        info = get_cover_sidecar_file(self.test_sidecar_path, 100, 100, 100)
+        assert type(info) == CoverSidecarFile
+
+    def test_load(self) -> None:
+        info = get_cover_sidecar_file(self.test_sidecar_path, 100, 100, 100)
+        assert info.read() == True
